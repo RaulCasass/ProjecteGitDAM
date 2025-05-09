60c905aa7c1f37940c79e99c34b36c53d7513532
Raul Casas <casascaparrosraul@gmail.com>
2025-05-09 09:40:56 +0200

    Primer commit

--- src/projectegitdam/ProjecteGitDAM.java
+++ src/projectegitdam/ProjecteGitDAM.java
@@ -15,6 +15,7 @@ public class ProjecteGitDAM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Provant Git amb Netbeans!");
     }
     
 }
